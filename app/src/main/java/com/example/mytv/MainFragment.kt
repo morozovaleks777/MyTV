dfa4ecb1530abe324888d66cc590b72ebf49b50d
oleksandr.o.morozov <morozovaleks@ukr.net>
2021-07-28 15:03:03 +0300

    rows2Adapter

--- app/src/main/java/com/example/mytv/MainFragment.kt
+++ app/src/main/java/com/example/mytv/MainFragment.kt
@@ -2,12 +2,12 @@ package com.example.mytv
 
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.os.Handler
+import android.util.DisplayMetrics
 import androidx.core.content.ContextCompat
+import androidx.leanback.app.BackgroundManager
 import androidx.leanback.app.BrowseSupportFragment
-import androidx.leanback.widget.ArrayObjectAdapter
-import androidx.leanback.widget.HeaderItem
-import androidx.leanback.widget.ListRow
-import androidx.leanback.widget.ListRowPresenter
+import androidx.leanback.widget.*
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 
@@ -16,6 +16,7 @@ import java.util.*
 
 
 class MainFragment : BrowseSupportFragment() {
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         setUI()
@@ -27,15 +28,20 @@ class MainFragment : BrowseSupportFragment() {
         val category2 = HeaderItem(1, "series")
 
         val rows1Adapter = ArrayObjectAdapter(CardPresenter())
+        val rows2Adapter = ArrayObjectAdapter(CardPresenter())
 
         rows1Adapter.add(SingleRowView("movie",Drawable.createFromPath("drawable/movie.png")))
         rows1Adapter.add(SingleRowView("moviemovie",Drawable.createFromPath("drawable/movie.png")))
         rows1Adapter.add(SingleRowView("moviemoviemovie",Drawable.createFromPath("drawable/movie.png")))
+        rows2Adapter.add(SingleRowView("series",Drawable.createFromPath("drawable/movie.png")))
+        rows2Adapter.add(SingleRowView("seriesseries",Drawable.createFromPath("drawable/movie.png")))
+        rows2Adapter.add(SingleRowView("seriesseriesseries",Drawable.createFromPath("drawable/movie.png")))
 
 
 
 val windowAdapter=ArrayObjectAdapter(ListRowPresenter())
-        windowAdapter.add( ListRow(category1,rows1Adapter))
+        windowAdapter.add(0, ListRow(category1,rows1Adapter))
+        windowAdapter.add(1, ListRow(category2,rows2Adapter))
         adapter = windowAdapter
     }
 
@@ -43,12 +49,12 @@ val windowAdapter=ArrayObjectAdapter(ListRowPresenter())
 
     private fun setUI() {
         title = getString(R.string.browse_title)
+        title
         headersState = BrowseSupportFragment.HEADERS_ENABLED
-        isHeadersTransitionOnBackEnabled = true
+        isHeadersTransitionOnBackEnabled =true
         brandColor = ContextCompat.getColor(requireActivity(), R.color.background_gradient_end)
-        searchAffordanceColor = ContextCompat.getColor(requireActivity(), R.color.search_opaque)
-    }
 
+    }
 
 
 }
